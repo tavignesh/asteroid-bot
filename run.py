d45e6425ed60f2ab00ab8aedc570639b118d6070
T.A.Vignesh <tavignesh2004@gmail.com>
2021-01-25 08:52:30 +0530

    dfghdf

--- run.py
+++ run.py
@@ -61,7 +61,7 @@ async def on_ready():
         await client.change_presence(status=discord.Status.do_not_disturb, activity=game)
     else:
         # game = discord.Game("With 『AG』》Vigne$hᴰᵉᵛ#8351 a/ help")
-        game = discord.Game("Quiz Ready !! Try a/ quiz help")
+        game = discord.Game("Quiz Ya its Ready !! Try a/ quiz help")
         await client.change_presence(status=discord.Status.idle, activity=game)
         # await client.change_presence(status=discord.Status.online, activity=game)
         # await client.change_presence(status=discord.Status.invisible, activity=game)
