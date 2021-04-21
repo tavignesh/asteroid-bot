90a9850124f95306d8dae9b33d9288b8c0d7f007
T.A.Vignesh <tavignesh2004@gmail.com>
2021-04-21 13:22:49 +0530

    hahaa

--- run.py
+++ run.py
@@ -487,7 +487,7 @@ async def on_message(message):
 # SYSTEM STATS
     if message.content.find("a/ cpu") != -1:
         await message.add_reaction("<a:ag_flyn_hrts_cyn:781395468978356235>")
-        if str(platform.system()) != "Windows":
+        if str(platform.system()) != "":
             pass
             cpupert = psutil.cpu_percent()
             cpun = psutil.cpu_count()
@@ -545,9 +545,41 @@ async def on_message(message):
             cpuswp = cpusw.split("=")[4]
             cpuswp = cpuswp.split(",")[0]
             # await message.channel.send(cpuidk)
+
             sysarch = platform.architecture()[0]
             nodsds = platform.node()
-            await message.channel.send(embed=discord.Embed(title="CPU STATS :tools:", description=f"\nNode: {nodsds}\n**Processor**\nArchetecture: {sysarch}\nCores = {cpun}\nSpeed = {cpui}Ghz\nTotal Usage = {cpupert}%\nCore 1 = {cpuo}%\nCore 2 = {cput}%\nCore 3 = {cpuy}%\nCore 4 = {cpuf}%\n\n**Swap Memory**\nTotal = {cpuswt} Gb\nUsed = {cpuswu} Gb\nPercentage = {cpuswp}%\nFree = {cpuswf} Gb\n\n**Battery**\nAvailable = {cpubat}% :battery: \nCharging = {cpubat}", color=0xFD9E01))
+            sysmchn = platform.system()
+
+            dist = platform.dist()
+            dist = " ".join(x for x in dist)
+
+            print("Memory Info: ")
+            with open("/proc/meminfo", "r") as f:
+                lines = f.readlines()
+
+            meminf1 = ("     " + lines[0].strip())
+            meminf2 = ("     " + lines[1].strip())
+
+            with open("/proc/cpuinfo", "r")  as f:
+                info = f.readlines()
+
+            syscpumo = "Error"
+            cpuinfo = [x.strip().split(":")[1] for x in info if "model name" in x]
+            for index, item in enumerate(cpuinfo):
+                syscpumo = ("    " + str(index) + ": " + item)
+
+            uptime = None
+            with open("/proc/uptime", "r") as f:
+                uptime = f.read().split(" ")[0].strip()
+            uptime = int(float(uptime))
+            uptime_hours = uptime // 3600
+            uptime_minutes = (uptime % 3600) // 60
+            servuptime = ("Uptime: " + str(uptime_hours) + ":" + str(uptime_minutes) + " hours")
+
+            with open("/proc/loadavg", "r") as f:
+                avgload = ("Average Load: " + f.read().strip())
+
+            await message.channel.send(embed=discord.Embed(title="CPU STATS :tools:", description=f"\nNode: {nodsds}\nLoad: {avgload}\n**Processor**\nArchitecture: {sysarch}\nModel: {syscpumo}\nCores = {cpun}\nSpeed = {cpui}Ghz\nTotal Usage = {cpupert}%\nCore 1 = {cpuo}%\nCore 2 = {cput}%\nCore 3 = {cpuy}%\nCore 4 = {cpuf}%\n\nOS: {sysmchn}\nDist: {dist}\nRAM:\ntotal: {meminf1}\nFree: {meminf2}\n**Swap Memory**\nTotal = {cpuswt} Gb\nUsed = {cpuswu} Gb\nPercentage = {cpuswp}%\nFree = {cpuswf} Gb\n\n**Battery**\nAvailable = {cpubat}% :battery: \nCharging = {cpubat}\n\nUptime: {servuptime}", color=0xFD9E01))
 
 # DATE TIME
     if message.content.find("a/ time") != -1:
