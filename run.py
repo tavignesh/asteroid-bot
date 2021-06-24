3e25c9ffd390f515d401b49a7ef89de059f4f3ac
T.A.Vignesh <tavignesh2004@gmail.com>
2021-06-24 12:41:42 +0530

    Update run.py

--- run.py
+++ run.py
@@ -43,16 +43,19 @@ porta = 8080
 
 app = Flask(__name__)
 
-# route
 @app.route('/')
-# route function
-def home():
-  # send 'hey!'
-  return 'hey!'
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
+    <img src="http://loremflickr.com/600/400" />
+    """.format(time=the_time)
 
 # listen
 if __name__ == "__main__":
-  app.run(port=porta)
+  app.run(debug=True, use_reloader=True)
   
   
   
