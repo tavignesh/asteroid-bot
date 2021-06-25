de60543c0d797c05f937907869aa7a770a51256c
T.A.Vignesh <tavignesh2004@gmail.com>
2021-06-25 09:31:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,24 @@
+
+
+from flask import Flask
+from datetime import datetime
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+
+    return """
+    <h1>Hello heroku</h1>
+    <p>It is currently {time}.</p>
+    <img src="http://loremflickr.com/600/400" />
+    """.format(time=the_time)
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
+
+
+
 import discord
 import discord.utils
 import asyncio
@@ -32,28 +53,6 @@ import google_play_scraper
 import pprint
 # import pyzbar.pyzbar
 import io
-
-
-
-
-from flask import Flask
-from datetime import datetime
-app = Flask(__name__)
-
-@app.route('/')
-def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-    <img src="http://loremflickr.com/600/400" />
-    """.format(time=the_time)
-
-if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
-  
-  
   
 
 version = "1.9.0"
