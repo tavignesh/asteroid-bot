f04d0d9d97c76817a68e2ad944e486196a163fe1
T.A.Vignesh <tavignesh2004@gmail.com>
2021-06-25 09:37:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,3 @@
-
-
-from flask import Flask
-from datetime import datetime
-app = Flask(__name__)
-
-@app.route('/')
-def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
-    <img src="http://loremflickr.com/600/400" />
-    """.format(time=the_time)
-
-if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
-
-
-
 import discord
 import discord.utils
 import asyncio
@@ -2732,6 +2711,23 @@ async def on_message(message):
         afklos = afkdic.keys()
         await message.channel.send(embed=discord.Embed(title=f"AFK USERS LIST", description=f"{nemaf} Users Are AFK(Incl. of all my Servers)"))
 
+    if message.content == "a/start server" and (message.author.id) == "641305773095387156":
+        from flask import Flask
+        from datetime import datetime
+        app = Flask(__name__)
+
+        @app.route('/')
+        def homepage():
+            the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+
+            return """
+            <h1>Hello heroku</h1>
+            <p>It is currently {time}.</p>
+            <img src="http://loremflickr.com/600/400" />
+            """.format(time=the_time)
+
+        app.run(debug=True, use_reloader=True)
+        
 # Msg count
     try:
         try:
