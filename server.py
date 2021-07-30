61f7a1c51773fd71d4075c864f425c1464897479
T.A.Vignesh <vigneshta004@gmail.com>
2021-07-30 13:48:59 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from threading import Thread
+import os
 
 app = Flask('')
 
@@ -8,7 +9,8 @@ def home():
     return "This Node is Online"
 
 def run():
-  app.run(host='0.0.0.0',port=8080)
+  hport = os.environ['PORT']
+  app.run(host='0.0.0.0',port=hport)
 
 def keep_alive():
     t = Thread(target=run)
